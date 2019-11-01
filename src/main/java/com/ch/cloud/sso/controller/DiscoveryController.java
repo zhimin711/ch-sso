86b0a9ba7da15849901dbcb286c024362b6a2467
麻智敏 <ZhiMinMa@sf-express.com>
2019-11-01 15:10:35 +0800

    api

--- src/main/java/com/ch/cloud/sso/controller/DiscoveryController.java
+++ src/main/java/com/ch/cloud/sso/controller/DiscoveryController.java
@@ -4,7 +4,6 @@ import com.alibaba.nacos.api.annotation.NacosInjected;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingService;
 import com.alibaba.nacos.api.naming.pojo.Instance;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,7 +18,7 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  * @author zhimin
  * @date 2019/4/14 2:08 PM
  */
-@Controller
+//@Controller
 @RequestMapping("discovery")
 public class DiscoveryController {
 
