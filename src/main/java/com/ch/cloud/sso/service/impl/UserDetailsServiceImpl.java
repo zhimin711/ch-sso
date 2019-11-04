6575a850156c4068c9af4071b9a37d6c2645fbac
仙_梦如尘 <zhimin711@sina.com>
2019-11-04 22:50:29 +0800

    调整3级菜单

--- src/main/java/com/ch/cloud/sso/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/com/ch/cloud/sso/service/impl/UserDetailsServiceImpl.java
@@ -186,7 +186,6 @@ public class UserDetailsServiceImpl implements UserDetailsService, IUserService 
         vo.setType(permission.getType());
         vo.setUrl(permission.getUrl());
         vo.setSort(permission.getSort()); String pid = CommonUtils.isEquals(NumS._0, permission.getParentId()) ? permission.getId().toString() : StringExtUtils.linkStr(",", permission.getParentId(), permission.getId().toString());
-
         if ("1".equals(permission.getType()) && pidMap.get(pid) != null) {
             List<MenuVo> menuVos = pidMap.get(pid).stream().map(e -> assemblyMenu(e, pidMap))
                     .sorted(Comparator.comparing(MenuVo::getSort)).collect(Collectors.toList());
