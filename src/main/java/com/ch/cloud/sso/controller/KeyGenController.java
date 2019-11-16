1d8afad0682f6872da2d8fff479abf3706e840b6
麻智敏 <ZhiMinMa@sf-express.com>
2019-11-16 11:22:49 +0800

    添加upms api

---
+++ src/main/java/com/ch/cloud/sso/controller/KeyGenController.java
@@ -0,0 +1,40 @@
+package com.ch.cloud.sso.controller;
+
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.ch.result.Result;
+import com.ch.result.ResultUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * desc:
+ *
+ * @author zhimin
+ * @date 2019/4/14 2:08 PM
+ */
+@RestController
+public class KeyGenController {
+
+    @NacosInjected
+    private NamingService namingService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    //    @GetMapping("serviceInstances")
+    public List<Instance> getInstances(@RequestParam String serviceName) throws NacosException {
+        return namingService.getAllInstances(serviceName);
+    }
+
+    @PostMapping("encrypt")
+    public Result<String> encrypt(@RequestParam String str) {
+        return ResultUtils.wrapFail(() -> passwordEncoder.encode(str));
+    }
+}
