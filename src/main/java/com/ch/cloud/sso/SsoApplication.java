8bfe79c068882815c0e1399f348f483b9cb50679
麻智敏 <ZhiMinMa@sf-express.com>
2019-11-26 15:02:30 +0800

    去掉数据源初始化

--- src/main/java/com/ch/cloud/sso/SsoApplication.java
+++ src/main/java/com/ch/cloud/sso/SsoApplication.java
@@ -3,11 +3,12 @@ package com.ch.cloud.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
