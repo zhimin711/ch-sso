60dabb0e2f068a1cddca76a8a3ccf991850f3e74
zhimin711 <zhimin711@sina.com>
2025-07-11 21:27:03 +0800

    refactor(api): 重构接口分组相关代码
    - 移除了 ApiGroupController 中未使用的 IApiPathService 注入
    - 新增 ApiGroupClient 接口，用于内部调用的 API 分组相关操作
    - 实现 ApiGroupClientController，提供模块列表和路径列表查询功能

---
+++ web/src/main/java/com/ch/cloud/api/controller/client/ApiGroupClientController.java
@@ -0,0 +1,61 @@
+package com.ch.cloud.api.controller.client;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ch.cloud.api.client.ApiGroupClient;
+import com.ch.cloud.api.domain.ApiGroup;
+import com.ch.cloud.api.domain.ApiOperationLog;
+import com.ch.cloud.api.domain.ApiPath;
+import com.ch.cloud.api.pojo.GroupPath;
+import com.ch.cloud.api.service.IApiGroupService;
+import com.ch.cloud.api.service.IApiOperationLogService;
+import com.ch.cloud.api.service.IApiPathService;
+import com.ch.result.Result;
+import com.ch.result.ResultUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author zhimin
+ * @date 2025/3/23 14:05
+ */
+@Tag(name = "api-group-client-controller", description = "接口分组(内部调用)")
+@RestController
+@RequestMapping("/c/api/group")
+public class ApiGroupClientController implements ApiGroupClient {
+
+    @Autowired
+    private IApiGroupService apiGroupService;
+
+    @Autowired
+    private IApiPathService apiPathService;
+
+    @Operation(summary = "查询模块列表", description = "查询模块列表")
+    @GetMapping("/modules")
+    @Override
+    public Result<GroupPath> modules(@RequestParam Long projectId) {
+        return ResultUtils.wrap(() -> {
+            List<ApiGroup> apiGroups = apiGroupService.listApiGroupByProjectId(projectId);
+            return BeanUtil.copyToList(apiGroups, GroupPath.class);
+        });
+    }
+
+    @Operation(summary = "查询路径列表", description = "查询路径列表")
+    @GetMapping("/paths")
+    @Override
+    public Result<GroupPath> paths(@RequestParam Long moduleId) {
+        return ResultUtils.wrap(() -> {
+            List<Long> pathIds = apiGroupService.listPathIdsByGroupId(moduleId);
+            List<ApiPath> apiPaths = apiPathService.listByIds(pathIds);
+            return BeanUtil.copyToList(apiPaths, GroupPath.class);
+        });
+    }
+}
