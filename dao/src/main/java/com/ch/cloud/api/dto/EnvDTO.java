c8feb54604a69bf5f9174686261d5eb3e6823b61
zhimin711 <zhimin711@sina.com>
2025-07-26 11:27:17 +0800

    feat(api): enhance EnvDTO with Swagger annotations for better API documentation
    - Added Swagger @Schema annotations to EnvDTO fields for improved API documentation.
    - Included descriptions for environment name, domain, prefix, and description fields.

--- dao/src/main/java/com/ch/cloud/api/dto/EnvDTO.java
+++ dao/src/main/java/com/ch/cloud/api/dto/EnvDTO.java
@@ -1,5 +1,6 @@
 package com.ch.cloud.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
@@ -7,9 +8,27 @@ public class EnvDTO {
 
     private Long id;
 
+    /**
+     * 环境名称
+     */
+    @Schema(description = "环境名称")
     private String name;
 
+    /**
+     * 环境域名
+     */
+    @Schema(description = "环境域名")
     private String domain;
 
+    /**
+     * 请求前缀
+     */
+    @Schema(description = "请求前缀")
     private String prefix;
+
+    /**
+     * 描述
+ */
+    @Schema(description = "描述")
+    private String description;
 }
